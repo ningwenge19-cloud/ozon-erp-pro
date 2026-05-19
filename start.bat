836f3aa6c55091d1fb4ed1e9d6081513d2cec68f
ningwenge19-cloud <ningwenge19@gmail.com>
2026-05-19 23:59:14 +0800

    Delete start.bat

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-if not exist ".venv\Scripts\python.exe" (
-  echo Creating local Python environment...
-  python -m venv .venv
-)
-".venv\Scripts\python.exe" -m pip install -r backend\requirements.txt
-cd backend
-"..\.venv\Scripts\python.exe" -m uvicorn main:app --reload --host 127.0.0.1 --port 8899
